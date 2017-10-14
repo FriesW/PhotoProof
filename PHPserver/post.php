2d5b442e7729feca4c20e71b27dbcd1a9f33b7b3
FriesW <william.james.fries@gmail.com>
2017-10-14 16:03:13 -0500

    Basic webserver for adding hashes
    Only using PHP because that is quick and available. There is a python
    version too, but I don't have open server ports on the bitcoin node.
    OP_RETURN from https://github.com/coinspark/php-OP_RETURN

---
+++ PHPserver/post.php
@@ -0,0 +1,16 @@
+<?php
+$key_file_name = "API_KEY.txt";
+
+require 'OP_RETURN.php';
+
+$key_file = fopen($key_file_name, "r") or die("Unable to open file! Server misconfigured.");
+$key = fread($key_file, filesize($key_file_name));
+fclose($key_file);
+
+if( isset($_POST["key"]) and isset($_POST["DATA"]) and $_POST["key"] === $key )
+{
+    $result = OP_RETURN_store($_POST["data"], $testnet);
+    echo $result['txids'];
+}
+
+?>
