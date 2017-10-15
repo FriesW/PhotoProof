c8f588a2b1f5c207c64ec8215f69340f20597b12
FriesW <william.james.fries@gmail.com>
2017-10-15 06:03:12 -0500

    Update Check.php
    Use OP_RETURN.php library instead of shell_exec(...).
    I am still not sure what the exact behavior of shell_exec is.

--- PHPserver/check.php
+++ PHPserver/check.php
@@ -1,14 +1,13 @@
 <?php
+require 'OP_RETURN.php';
 require 'key_check.php';
 
-$cmd_base = "bitcoin-cli -testnet gettransaction ";
-$error_key = "Invalid";
 
 if( isset($_POST["txid"]) and key_check() and preg_match( "/^[a-fA-F0-9]{65}$/", $_POST["txid"]) )
 {
-    $result = shell_exec($cmd_base + $_POST["txid"]);
-    if( strpos($result, $error_key) == false )
-        echo json_decode($result)->{'confirmations'};
+    $result = OP_RETURN_bitcoin_cmd('gettransaction', true, $_POST["txid"]);
+    if( array_key_exists("confirmations", $result) )
+        echo $result['confirmations'];
     else
         echo "error"; //Response code?
 }
