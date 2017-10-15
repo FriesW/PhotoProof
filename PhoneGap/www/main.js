667b54e2f863ff6865e3736d4644d01931a3477c
FriesW <william.james.fries@gmail.com>
2017-10-15 06:11:23 -0500

    Remove debug

--- PhoneGap/www/main.js
+++ PhoneGap/www/main.js
@@ -93,7 +93,6 @@ function getPhoto() {
 function updateItem(txid)
 {
     get_txid(txid, function(depth){
-        alert("got " + depth);
         depth = parseInt(depth);
         if( !isNaN(depth) )
         {
